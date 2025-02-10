82cee27c63e21a77a14981eb655c4e95d56e4207
ReBeX <cswwwx@gmail.com>
2025-02-10 17:37:13 +0800

    Update static.yml

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/ui-csw/',
   vite: {
     plugins: [
       UnoCSS(),
