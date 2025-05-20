2e36e66f70ce08eddcce8c5c425b27927a4dcad9
ReBeX <cswwwx@gmail.com>
2025-05-20 15:42:57 +0800

    fix: card6 style

--- src/components/Card/Card6.vue
+++ src/components/Card/Card6.vue
@@ -1,14 +1,14 @@
 <!--
  * @Date: 2024-04-29 09:44:29
  * @LastEditors: ReBeX cswwwx@gmail.com
- * @LastEditTime: 2025-05-20 14:31:37
+ * @LastEditTime: 2025-05-20 15:42:12
  * @FilePath: /ui-csw/src/components/Card/Card6.vue
  * @Description: 卡片 - 模板6
  * @Ref: https://github.com/fengtianxi001/MF-TurbineMonitor
 -->
 
 <template>
-  <div class="layout-panel animate animate__bounceIn">
+  <div class="layout-panel animate animate__bounceIn text-zx-1">
     <div class="panel-header">
       <slot name="title">
         标题
@@ -31,7 +31,7 @@
 }
 
 .panel-header {
-  @apply flex items-center h-[65px] pl-[58px] w-full pb-[20px];
+  @apply flex items-center h-[65px] pl-[58px] w-full pb-[20px] pr-2;
   background-image: url('../../assets/img/card6_title.png');
   background-repeat: no-repeat;
   background-size: 100% 100%;
